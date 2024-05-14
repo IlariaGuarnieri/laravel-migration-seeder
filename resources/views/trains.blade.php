a94f081483880b87dc7ab9a3f7fefafe8e44d7ba
IlariaGuarnieri <ilaria.guarnieri@gmail.com>
2024-05-14 16:43:52 +0100

    [add] inizio bonus 1

--- resources/views/trains.blade.php
+++ resources/views/trains.blade.php
@@ -10,7 +10,8 @@
         <th scope="col">COMPAGNIA</th>
         <th scope="col">NUMERO TRENO</th>
         <th scope="col">DESTINAZIONE</th>
-        <th scope="col">ORARIO</th>
+        <th scope="col">ORARIO DI PARTENZA</th>
+        <th scope="col">ORARIO DI ARRIVO</th>
         <th scope="col">RITARDO</th>
         <th scope="col">CANCELLATO</th>
       </tr>
@@ -22,11 +23,16 @@
         <th scope="row">{{$train->company}}</th>
         <th scope="row">{{$train->train_code}}</th>
         <th scope="row">{{$train->arrival_station}}</th>
-        <th scope="row">{{$train->arrival_station}}</th>
-        <th scope="row">{{$train->arrival_station}}</th>
+        <th scope="row">{{$train->departure_time}}</th>
+        <th scope="row">{{$train->arrival_time}}</th>
+        <th scope="row">{{$train->minutes_late}}</th>
+        <th scope="row">{{$train->is_cancelled}}</th>
       </tr>
       @endforeach
     </tbody>
   </table>
 
+<div class="container">
+    {{$trains->links()}}
+</div>
   @endsection
